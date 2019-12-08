7110ea0033d7429b7c0321c353623f2543d46f8b
andrecarrsd2008 <andrecarrsd2008>
2019-12-07 22:27:45 -0800

    Added CSS & Radium Package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -45,7 +46,8 @@ deletePersonHandler = (personIndex) => {
 
     render () {
       const style = {
-        backgroundColor: 'white',
+        backgroundColor: 'green',
+        color: 'white',
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
@@ -67,12 +69,22 @@ deletePersonHandler = (personIndex) => {
             })} 
           </div> 
         );
-      }
 
-      return (
+        style.backgroundColor = 'red';
+       }
+
+       const classes = []; 
+       if (this.state.persons.length <= 2) {
+         classes.push('red'); // classes = ['red']
+       }
+       if (this.state.persons.length <= 1) {
+         classes.push('bold'); // classes = ['red', 'bold']
+       }
+       
+       return (
         <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -83,5 +95,5 @@ deletePersonHandler = (personIndex) => {
   } 
 }
 
-export default App;
+export default Radium(App);
 
