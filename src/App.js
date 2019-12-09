971a6a253a5ec5dc171230787d2b12df571d1942
andrecarrsd2008 <andrecarrsd2008>
2019-12-08 20:16:39 -0800

    Uploaded Style Components removed Radium

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium from 'radium';
+
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  
+  &:hover: {
+    background-color: lightgreen;
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -51,7 +66,11 @@ deletePersonHandler = (personIndex) => {
         font: 'inherit',
         border: '1px solid blue',
         padding: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover': {
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
       };
 
       let persons  = null;
@@ -71,6 +90,10 @@ deletePersonHandler = (personIndex) => {
         );
 
         style.backgroundColor = 'red';
+        style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }
        }
 
        const classes = []; 
@@ -82,18 +105,19 @@ deletePersonHandler = (personIndex) => {
        }
        
        return (
+        
         <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button 
-        style={style}
-        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <StyledButton onClick={this.togglePersonsHandler}>
+         Toggle Persons
+        </StyledButton>
         {persons}
       </div>
-    );
+     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   } 
 }
 
-export default Radium(App);
+export default App;
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './Person.css';
+// import './Person.css';
+const StyledDiv = styled.div`
+width: 60%;
+margin: auto;
+border: 1px solid #eee;
+box-shadow: 0 2px 3px #ccc;
+padding: 16px;
+text-align: center;
+
+@media (min-width: 500px) {
+   width: 450px;
+}
+`;
 
 const person = ( props ) => {
+    // const style = {
+    //     '@media (min-width: 500px)': {
+    //        width: '450px'
+    //     }
+    // };
     return (
-        <div className="Person">
+        // <div className="Person" style={style}>
+        <div>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
