7c217c6a20cca33944615fb332bceb6ad5cb8cc8
andrecarrsd2008 <andrecarrsd2008>
2019-12-15 19:31:27 -0800

    Add Several New Folders & Files

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-
-
 import classes from './App.css';
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
 
 
 class App extends Component {
@@ -49,42 +47,21 @@ class App extends Component {
       let persons  = null;
       let btnClass = '';
 
-      if (this.state.showPersons) {
+      if ( this.state.showPersons ) {
         persons = (
           <div>
-              {this.state.persons.map((person, index) => {
-                return (
-                 <Person 
-                   click={() => this.deletePersonHandler(index)}
-                   name={person.name} 
-                   age={person.age} 
-                   key={person.id} 
-                   changed={event => this.nameChangedHandler(event, person.id)} 
-                />
-                );
-            })} 
+            <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler} />
           </div> 
         );
-
-       btnClass = classes.Red;
       }           
-       const assignedClasses = []; 
-       if (this.state.persons.length <= 2) {
-         assignedClasses.push(classes.red); // classes = ['red']
-       }
-       if (this.state.persons.length <= 1) {
-         assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-       }
        
-       return (
+      return (
         <div className={classes.App}>
-        <h1>Hi, I'm a React App</h1>
-        <p className={assignedClasses.join(' ')}>This is really working!</p>
-        <button className={btnClass} onClick={this.togglePersonsHandler}>
-         Toggle Persons
-        </button>
-        {persons}
-      </div>
+          {persons}
+        </div>
      );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   } 
