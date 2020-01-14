6d9ee1e3ea191af8c1fe421b0d5d62948f60906b
andrecarrsd2008 <andrecarrsd2008>
2020-01-13 20:47:43 -0800

    Cleaned up Functions & added Auxiliary

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 //   static getDerivedStateFromProps(props, state) {
 //         console.log('[Persons.js] getDerivedStateFromProps'); 
 //     return state;
@@ -12,10 +12,18 @@ class Persons extends Component {
 //    console.log('[Persons.js] componentWillReceiveProps', props);
 //  }
 
-  shouldComponentUpdate(nextProps, nextState) {
-      console.log('[Persons.js] shouldComponentUpdate'); 
-      return true;  
-  }
+//   shouldComponentUpdate(nextProps, nextState) {
+//       console.log('[Persons.js] shouldComponentUpdate'); 
+//       if (
+//           nextProps.persons !== this.props.persons || 
+//           nextProps.changed !== this.props.changed || 
+//           nextProps.clicked !== this.props.clicked
+//       )  {
+//           return true;
+//       } else {
+//           return false;
+//       } 
+//   }
 
   getSnapsshotBeforeUpdate(prevProps, prevState) {
       console.log('[Persons.js] getSnapshotBeforeUpdate'); 
@@ -34,7 +42,7 @@ class Persons extends Component {
   componentWillUnmount() {
     console.log('[Persons.js] componentWillUnmount');
   }
-  
+
   render() {
     console.log('[Persons.js] rendering...');
       return this.props.persons.map((person, index) => {
